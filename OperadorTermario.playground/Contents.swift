165ee78e1def7a73c3b4fa9cf16ccd8889c4b179
Suemy-xjl <60667688+Suemy-xjl@users.noreply.github.com>
2022-02-07 23:49:53 -0600

    Add files via upload

---
+++ OperadorTermario.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Foundation
+
+//Operador termario//
+
+func isPar(number: Int) -> Bool{
+    
+    let status : Bool = (number % 2 == 0 ) ? true : false
+    
+    
+    //if number % 2 == 0{
+    //  return true
+    //}
+    return status
+}
